181823b824b6486a93470b67c19ffca075192626
“davidmagbee” <david.magbee@me.com>
2023-12-13 15:50:01 -0500

    est db req auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+import { auth } from "@clerk/nextjs"
 import Link from "next/link"
 
-export default function Home() {
+export default async function Home() {
+  const {userId} = await auth()
+  let href = userId ? '/journal' : '/new-user'
+
   return <div className='w-screen h-screen bg-black flex justify-center items-center text-white'>
     <div className="w-full max-w-[600px] mx-auto">
       <h1 className="text-6xl mb-4">Journal App</h1>
       <p className="text-2xl text-white/60 mb-4">This is a journal project for tracking your mood as you journal using AI.
       </p>
       <div>
-        <Link href='/journal'>
+        <Link href={href}>
           <button className="bg-blue-600 px-4 py-4 rounded-lg text-xl">get started</button>
         </Link>
       </div>

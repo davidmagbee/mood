6e90d7d55638bf39d6f5c1659db70066718df50b
“davidmagbee” <david.magbee@me.com>
2023-12-28 11:09:38 -0500

    editor and entry updates

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,9 @@ const DashboardLayout = ({ children }) => {
     return (
         <div className="h-screen w-screen relative">
             <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
-                Mood
+                <div className="px-4 my-4">
+                    <span className="text-3xl">MOOD</span>
+                </div>
             </aside>
             <div className="ml=[200px]">
                 <header className="h-[60px] border-b border-black/10">

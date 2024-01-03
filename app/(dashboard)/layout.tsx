3baed3d0dd5df8cfc51aea080b869273788a679b
“davidmagbee” <david.magbee@me.com>
2024-01-03 12:43:20 -0500

    sidebar UI

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,16 +3,18 @@ import { UserButton } from "@clerk/nextjs"
 const DashboardLayout = ({ children }) => {
     return (
         <div className="h-screen w-screen relative">
-            <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
+            <aside className="absolute left-0 top-0 h-full w-[200px] border-r border-black/10">
                 <div className="px-4 my-4">
                     <span className="text-3xl">MOOD</span>
                 </div>
             </aside>
-            <div className="ml=[200px]">
+            <div className="ml-[200px] ">
                 <header className="h-[60px] border-b border-black/10">
-                    <div className="h-full w-full px-6 flex items-center justify-end">
-                        <UserButton />
-                    </div>
+                    <nav className="px-4 h-full">
+                        <div className="h-full w-full px-6 flex items-center justify-end">
+                            <UserButton />
+                        </div>
+                    </nav>
                 </header>
                 <div className="h-[calc(100vh-60px)]">{children}</div>
             </div>
